5c2200405d2dcd1879b91a5f47801185de42e7d0
lilmuff1 <100513345+lilmuff1@users.noreply.github.com>
2022-02-27 13:47:45 +0300

    Update js.js

--- js.js
+++ js.js
@@ -3,7 +3,6 @@ button.addEventListener("click", (e) => {
     let link = document.querySelector(".link").value
     if (link.length > 0){
     link = `brawlstars://webview?page=${link};popup_title=TG:@prostobrawl2`
-    Console.log(link)
-    window.location.href = `brawlstars://webview?page=https://qiwi.com/n/LEXA7386;popup_title=TG:@prostobrawl2`
-    }
+    console.log(link)
+   } 
 })
